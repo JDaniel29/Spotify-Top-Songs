db65f1b65856a439bb50191373fd7352165f713a
JOnathan Daniel <gcagrad19@gmail.com>
2020-07-13 23:21:05 -0400

    First iteration of set_client_variables.sh

---
+++ set_client_variables.sh
@@ -0,0 +1,5 @@
+# Replace with the values for your Spotify Application
+
+export SPOTIPY_CLIENT_ID=client_id_here
+export SPOTIPY_CLIENT_SECRET=client_secret_here
+export SPOTIPY_REDIRECT_URI=redirect_uri_here
